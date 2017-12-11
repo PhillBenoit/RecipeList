3e72711c0286919b8e86e58fe31caa4b0fbf08b0
PhillBenoit <PhillBenoit@users.noreply.github.com>
2017-12-11 15:31:53 -0700

    added widget

--- app/src/main/java/com/example/user1/recipelist/Activities/SingleStep.java
+++ app/src/main/java/com/example/user1/recipelist/Activities/SingleStep.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
 import android.view.View;
 import android.widget.Button;
@@ -28,7 +29,7 @@ import com.google.android.exoplayer2.util.Util;
  *
  */
 
-public class SingleStep extends Activity {
+public class SingleStep extends AppCompatActivity {
 
     int id, step, number_of_steps, min_step = 0;
 
